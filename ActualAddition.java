778ea5ae8c3f7d7185e14b1333c95fe5ce809caf
Sebastian Meijerhof <smeijerh@sfu.ca>
2023-01-02 10:48:26 -0800

    Test problem submission

---
+++ ActualAddition.java
@@ -0,0 +1,17 @@
+package MASHProgContest;
+import java.util.Scanner;
+
+ class ActualAddition {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int len = s.nextInt();
+        int total = 0;
+
+        for(int i = 0; i < len; i++) {
+            total += s.nextInt();
+        }
+
+        System.out.println(total);
+    }    
+}
